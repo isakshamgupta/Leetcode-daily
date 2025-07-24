a12fc29cccfa57b9ad1cc777ce81872fe36c2b04
Saksham Gupta <cutesakshamgupta@gmail.com>
2025-07-24 22:38:38 +0530

    MD-24-July

--- 24-july-2025/q1.cpp
+++ 24-july-2025/q1.cpp
@@ -1,1 +1,20 @@
-##leetcode question soln:-
+// 2894. Divisible and Non-divisible Sums Difference
+
+class Solution {
+public:
+    int differenceOfSums(int n, int m) {
+        int num1 = 0;
+        int num2 = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % m == 0) {
+                num2 += i;
+            } else {
+                num1 += i;
+            }
+        }
+        cout << num1 << " ";
+        cout << num2 << " ";
+        return num1 - num2;
+    }
+};
